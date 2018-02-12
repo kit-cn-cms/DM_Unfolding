6cee3de5d50fac6443570dba19072db3545e2e91
sebwieland <wielandseb@googlemail.com>
2018-02-12 11:12:35 +0100

    HitFilling working

--- interface/HistMaker.hpp
+++ interface/HistMaker.hpp
@@ -9,9 +9,15 @@
 #include "TGraph.h"
 #include "math.h"
 #include "TStyle.h"
+#include "TChain.h"
+#include "TString.h"
 // #include "gstyle.h"
 // #include <sys/stat.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
 
 
 using namespace std;
@@ -22,11 +28,28 @@ using namespace std;
 class HistMaker
 {
 public:
-	void GetInputFileList(const char* path, std::string type);
+	std::vector<TString> GetInputFileList(TString path , TString type);
 	void SetUpHistos();
-	TFile histos;
+	void ParseConfig();
+	void FillHistos(TChain* chain);
+	void MakeHistos();
+	TChain* ChainFiles();
+	TH1F* GetHisto(TString name);
 
 
+	TFile* histos;
+	TString genvar;
+	TString recovar;
+	TString path;
+	TString variation;
+	int nBins_Gen;
+	int nBins_Reco;
+	int xMin;
+	int xMax;
+	double nMax;
+	std::vector<TString> filelist;
 
+	TH1F h_Gen;
+	TH1F h_Reco;
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,27 @@ int main()
 {
 	boost::property_tree::ptree pt;
 	boost::property_tree::ini_parser::read_ini("Config/DMConfig.ini", pt);
-	
+
 	string path = pt.get<std::string>("Sample1.path");
 	std::cout << path << std::endl;
 
-
-
-
 	HistMaker histomaker;
-	// const char* path = "/nfs/dust/cms/user/mwassmer/DarkMatter/ntuples";
+	histomaker.MakeHistos();
 
-	histomaker.GetInputFileList(path.c_str(),"nominal");
-	histomaker.SetUpHistos();
-	cout << "test" << endl;
+	TString genvar = pt.get<string>("vars.gen");
+	TString recovar = pt.get<string>("vars.reco");
+
+	TH1F* gen=histomaker.GetHisto(genvar);
+	TH1F* reco=histomaker.GetHisto(recovar);
+	TH1F* A=histomaker.GetHisto("A");
+
+
+
+	gen->Print();
+	int n =gen->Integral();
+	cout << n << endl;
+	reco->Print();
+	A->Print();
 
 
 	return (0);
